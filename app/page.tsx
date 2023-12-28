490d7d637bb38b44d16844be55f3548bef9b3a50
GiorgiIlariani <lmaisuradze3333@gmail.com>
2023-12-28 11:08:33 +0400

    fetching categories on client side

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
-import React from "react";
 import Content from "@/components/content";
 import Header from "@/components/shared/Header";
 import Hero from "@/components/shared/Hero";
-import { fetchCategories } from "@/lib/actions/fetchCategories";
 
-const Page = async () => {
-  const categories = await fetchCategories();
-
+const Page = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <div className="w-full px-4">
         <Hero />
-        <Content categories={categories} />
+        <Content />
       </div>
     </div>
   );
--- components/content/index.tsx
+++ components/content/index.tsx
@@ -6,30 +6,40 @@ import { fetchBlogs } from "@/lib/actions/fetchBlogs";
 import { useEffect, useState } from "react";
 import CategoriesList from "./categories";
 import FetchBlogsLoading from "../loadings/fetchAllBlogsLoading";
-import { fetchCategories } from "@/lib/actions/fetchCategories";
 
-const MainContent = ({ categories }: { categories: CategoryTypes[] }) => {
+const MainContent = () => {
   const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
-  // const [categories, setCategories] = useState<CategoryTypes[]>([]);
+  const [categories, setCategories] = useState<CategoryTypes[]>([]);
   const [blogs, setBlogs] = useState<BlogsTypes[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchContent = async () => {
+    const fetchCategories = async () => {
+      try {
+        const data = await fetchBlogs(selectedCategories);
+
+        setCategories(data);
+      } catch (error) {
+        console.error("Error fetching content:", error);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  useEffect(() => {
+    const fetchBlogsData = async () => {
       try {
         setIsLoading(true);
         const data = await fetchBlogs(selectedCategories);
-        // const categories = await fetchCategories();
 
         setBlogs(data);
-        // setCategories(categories);
       } catch (error) {
         console.error("Error fetching content:", error);
       } finally {
         setIsLoading(false);
       }
     };
-    fetchContent();
+    fetchBlogsData();
   }, [selectedCategories]);
 
   return (
