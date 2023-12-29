4b6c9fbcf6158df60b0b8f05ffed2c892a0e31f8
GiorgiIlariani <lmaisuradze3333@gmail.com>
2023-12-29 10:20:06 +0400

    trying to fix error

--- app/blogDetails/[id]/page.tsx
+++ app/blogDetails/[id]/page.tsx
@@ -10,15 +10,15 @@ const Page = async ({ params }: { params: { id: string } }) => {
   const blogDetails = await fetchEachBlog(Number(params.id));
   const data = await fetchBlogs([], 1);
 
-  const { title, description, image, publish_date, categories, author, email } =
-    blogDetails;
+  // const { title, description, image, publish_date, categories, author, email } =
+  //   blogDetails;
 
-  // Filter out the current blog from the list of similar blogs
+  // Filter out the currentblogDetails?. blog from the list of similar blogs
   const filteredSimilarBlogs = data.data.filter(
     (blog: BlogsTypes) =>
       blog.id !== Number(params.id) && // Exclude the current blog
       blog.categories.some((category) =>
-        categories.some((c: CategoryTypes) => c.id === category.id)
+        blogDetails?.categories.some((c: CategoryTypes) => c.id === category.id)
       )
   );
 
@@ -28,13 +28,13 @@ const Page = async ({ params }: { params: { id: string } }) => {
       <div className="py-10">
         <div className="max-w-[720px] mx-auto flex flex-col my-10 px-6 sm:px-2">
           <BlogDetails
-            title={title}
-            description={description}
-            image={image}
-            publish_date={publish_date}
-            categories={categories}
-            author={author}
-            email={email}
+            title={blogDetails?.title}
+            description={blogDetails?.description}
+            image={blogDetails?.image}
+            publish_date={blogDetails?.publish_date}
+            categories={blogDetails?.categories}
+            author={blogDetails?.author}
+            email={blogDetails?.email}
           />
         </div>
         <div className="w-full px-4">
